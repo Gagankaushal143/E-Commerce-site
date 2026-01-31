c42fd52c195b8582c40d1a9069553afa772947be
Gagan Kaushal <gagankaushal143@gmail.com>
2026-01-31 18:11:52 +0530

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
     <footer>
-        <div className="py-6">
-            <hr className="text-pink-300 p-2"/>
+        <div className="pb-6">
+            <hr className="text-pink-300 pb-2"/>
             <p className="text-center text-gray-500">
                 © {new Date().getFullYear()} 
                 <p className="text-lg py-2 text-pink-400 italic font-bold">Swift<span className="text-purple-600">Cart</span> </p>
